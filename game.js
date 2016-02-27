42f0ec13092e78a121ab2599919ba93d469b1203
Dara <dara.mchugh@gmail.com>
2016-02-27 20:39:29 +0000

    changed comprehensions to arrow functions

--- game.js
+++ game.js
@@ -1,11 +1,13 @@
 var game = (function(){
 
     var intersection = function(setA, setB){
-	return new Set([x for (x of setA) if (setB.has(x))]);
+	var el_in_b = (x => setB.has(x));
+	return new Set([...setA].filter(el_in_b));
     }
     
     var difference = function(setA, setB){
-	return new Set([x for (x of setA) if (!setB.has(x))]);
+	var el_not_in_b = (x => !setB.has(x));
+	return new Set([...setA].filter(el_not_in_b));
     }
     var Game = function(lives, word, guesses){	
 	this.lives = lives;
@@ -15,7 +17,7 @@ var game = (function(){
 	this.correct_guesses = intersection(this.guesses, this.word_letters);
 	this.incorrect_guesses = difference(this.guesses, this.word_letters);	
 	this.is_dead = this.lives <= this.incorrect_guesses.size;
-	this.has_won = this.correct_guesses.size == (this.word_letters.size);
+	this.has_won = this.correct_guesses.size == this.word_letters.size;
 	
 	this.addGuess = function(letter){
 	    return Game(this.lives, this.word,
