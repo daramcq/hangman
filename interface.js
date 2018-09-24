72c001f46b429551de6ad85339fe28b2ddefdc44
Dara McHugh <dara.mchugh@gmail.com>
2018-09-24 22:52:01 +0100

    More indentation

--- interface.js
+++ interface.js
@@ -1,48 +1,48 @@
 var interface = (function(){
 
     var drawScreen = function(screen){
-	$('#game-message').html(screen.msg);
-	$('#gallows').html(screen.gallows);
-	$('#display-word').html(screen.word);	
-	$('#guesses').html(screen.wrong_guesses);
+	    $('#game-message').html(screen.msg);
+	    $('#gallows').html(screen.gallows);
+	    $('#display-word').html(screen.word);	
+	    $('#guesses').html(screen.wrong_guesses);
     };
     var chooseWord = function(words){
-	var rand = Math.floor(Math.random() * words.length);
-	return words[rand];
+	    var rand = Math.floor(Math.random() * words.length);
+	    return words[rand];
     };
     var isLetter = function(str){
         return str.length === 1 && str.match(/[a-z]/i).length>0;
     };    
 
     var init = function(){
-	var lives = 5;
-	var words = ['munificent','interlude', 'cavalcade',
-		     'promontory', 'moribund', 'pabulum',
-		     'sinecure', 'vestments','illegitimate',
-		     'consummate', 'dialectical', 'corpulent',
-		     'duopoly', 'oligarchic', 'gavel',
-		     'edifice', 'verily', 'varlet',
-		     'venal', 'sacrosanct', 'purgative',
-		     'substantiate', 'corporeal', 'plenitude'];
-	var word = chooseWord(words);
+	    var lives = 5;
+	    var words = ['munificent','interlude', 'cavalcade',
+		             'promontory', 'moribund', 'pabulum',
+		             'sinecure', 'vestments','illegitimate',
+		             'consummate', 'dialectical', 'corpulent',
+		             'duopoly', 'oligarchic', 'gavel',
+		             'edifice', 'verily', 'varlet',
+		             'venal', 'sacrosanct', 'purgative',
+		             'substantiate', 'corporeal', 'plenitude'];
+	    var word = chooseWord(words);
         var guesses = new Set([]);
-	var game_obj = game.Game(lives, word, guesses);
-	var scr = screen.Screen(game_obj);
-	drawScreen(scr);
-	$(document).keypress(function(e){	
-	    handleInput(e);	  
+	    var game_obj = game.Game(lives, word, guesses);
+	    var scr = screen.Screen(game_obj);
+	    drawScreen(scr);
+	    $(document).keypress(function(e){	
+	        handleInput(e);	  
         });
-	var handleInput = function(e){
-	    if (!isLetter(e.key) || (game_obj.guesses.has(e.key))){
-		return;
+	    var handleInput = function(e){
+	        if (!isLetter(e.key) || (game_obj.guesses.has(e.key))){
+		        return;
 	    }
-	    if (game_obj.is_dead || game_obj.has_won){
-		return;
+	        if (game_obj.is_dead || game_obj.has_won){
+		        return;
 	    }
-	    game_obj = game_obj.addGuess(e.key);
-	    scr = screen.Screen(game_obj);
-	    drawScreen(scr);		
-	}
+	        game_obj = game_obj.addGuess(e.key);
+	        scr = screen.Screen(game_obj);
+	        drawScreen(scr);		
+	    }
     }    
     return { init : init };
 })();
--- screen.js
+++ screen.js
@@ -1,32 +1,32 @@
 var screen = (function(){
 
     var getWord = function(game){
-	var getLetter= function(letter){
-	    return (game.guesses.has(letter) ? letter : "_");
+	    var getLetter= function(letter){
+	        return (game.guesses.has(letter) ? letter : "_");
 	}
-	var display_letters = game.word.split('').map(getLetter);
-	return display_letters.join(' ');
+	    var display_letters = game.word.split('').map(getLetter);
+	    return display_letters.join(' ');
     }
     
     var getMsg = function(game){
-	var msg = '';
-	if (game.has_won){
-	    msg = 'Congrats! You won!'
-	}else if (game.is_dead){
-	    msg = 'Oh no, you\'re dead!';
-	}else{
-	    msg = 'Ok, press a key!';
-	}
-	return msg;
+	    var msg = '';
+	    if (game.has_won){
+	        msg = 'Congrats! You won!'
+	    }else if (game.is_dead){
+	        msg = 'Oh no, you\'re dead!';
+	    }else{
+	        msg = 'Ok, press a key!';
+	    }
+	    return msg;
     }
 
     var Screen = function(game){	
-	this.msg = getMsg(game);
-	this.word = getWord(game);
-	this.gallows = gallows[game.incorrect_guesses.size];
-	this.wrong_guesses = [...game.incorrect_guesses].join(' ');
+	    this.msg = getMsg(game);
+	    this.word = getWord(game);
+	    this.gallows = gallows[game.incorrect_guesses.size];
+	    this.wrong_guesses = [...game.incorrect_guesses].join(' ');
 
-	return this;
+	    return this;
     }    
     return { Screen : Screen };
 })()
